499d27e71cea41d4de057af70fc649c74f9ba2c5
Dipto Karmakar <diptokmk47@gmail.com>
2018-10-19 01:10:38 +0600

    Cerate tic-tac-toe with python      - Create main.py      - Create game.py

---
+++ classes/game.py
@@ -0,0 +1,91 @@
+from pyfiglet import figlet_format
+
+
+class Game:
+    def __init__(self):
+        self.box_list = []
+        for i in range(1, 10):
+            self.box_list.append(i)
+        self.__combination = ("036", "147", "258", "012",
+                              "345", "678", "048", "246")
+
+    @staticmethod
+    def show_title():
+        print(figlet_format('Tic - Tac - Toe', font='doom'))
+        print('==========================================================================')
+        print('* * * Created by Dipto Karmakar || Created Date 18th September 2018 * * *')
+        print(
+            '==========================================================================\n')
+
+    def draw_board(self):
+        box_len = len(self.box_list)
+        print("\nGame Board\n")
+        for i in range(0, box_len):
+            print(self.box_list[i], end="")
+            if (i + 1) % 3 == 0:
+                print('\n---------')
+            else:
+                print(' | ', end="")
+
+    def plyars_info_collect(self):
+        print("\nPlayer One Info")
+        player_one = input("Your name: ")
+        player_one_sign = input(
+            "Your Sign [Any character you want]: ")
+        print("\nPlayer two Info")
+        player_two = input("Your name : ")
+        player_two_sign = input(
+            "Your Sign [Any character you want]: ")
+        if player_one == player_two or player_one_sign == player_two_sign:
+            print(
+                "\n[ Found same information about players.Please insert diffrent info. ]\n")
+            self.plyars_info_collect()
+        return {player_one: player_one_sign,
+                player_two: player_two_sign}
+
+    def update_box_list(self, select_box, sign):
+        self.box_list[select_box] = sign
+
+    def game_move(self, player_obj):
+        box_choose = input(
+            "{} insert box number from above game board > ".format(player_obj.name))
+        if not "1" <= box_choose <= "9" and self.box_list[int(box_choose)-1] is box_choose:
+            print(
+                "\nInvalid input.Please check gameboard avaiable number and input again\n")
+            self.game_move(player_obj)
+        player_obj.choices.append(int(box_choose) - 1)
+        self.update_box_list(int(box_choose) - 1, player_obj.sign)
+        player_obj.step += 1
+        if self.game_state_check(player_obj):
+            self.game_end()
+        self.draw_board()
+
+    def game_state_check(self, player_obj):
+        choose_str = ''.join(map(str, player_obj.choices))
+        choose_length = len(choose_str)
+        if choose_length >= 3:
+            for val in self.__combination:
+                for i in range(choose_length-2):
+                    if val is choose_str[i:i+3]:
+                        print("{} won! in {} steps".format(
+                            player_obj.name, player_obj.step))
+                        return True
+        if choose_length >= 4:
+            return False
+
+    def game_start(self):
+        self.draw_board()
+
+    def game_end(self):
+        print("Thanks for playing!")
+
+
+class Player:
+    def __init__(self, name, sign):
+        self.name = name
+        self.sign = sign
+        self.choices = []
+        self.step = 0
+
+    def show_player_info(self):
+        print("{} choose {} sign".format(self.name, self.sign))
---
+++ main.py
@@ -0,0 +1,27 @@
+from classes.game import *
+# TODO: Remove this import
+# from classes.test import *
+new_game = Game()
+new_game.show_title()
+players_info = new_game.plyars_info_collect()
+players = []
+print("_____________\n")
+for player in players_info:
+    players.append(Player(player, players_info[player]))
+    players[len(players) - 1].show_player_info()
+print("\nBest of luck {} and {}".format(players[0].name, players[1].name))
+
+new_game.game_start()
+for i in range(5):
+    # print(i+1)
+    if i == 4:
+        new_game.game_move(players[0])
+        break
+    else:
+        new_game.game_move(players[0])
+    if i == 4:
+        new_game.game_move(players[1])
+        break
+    else:
+        new_game.game_move(players[1])
+new_game.game_end()
