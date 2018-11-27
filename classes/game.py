66887edbddfd8daa140d630982b5d3074e003911
dipto0321 <diptokmk47@gmail.com>
2018-11-27 23:46:32 +0600

    Try to make win logic

--- classes/game.py
+++ classes/game.py
@@ -1,13 +1,13 @@
 from pyfiglet import figlet_format
-from classes.usefull import *
+from .usefull import *
+from itertools import permutations as perm
 
 
 class Game:
     def __init__(self):
         self.box_list = [i for i in range(1, 10)]
-        self.__combination = ([0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2],
-                              [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6])
-        self.stop = False
+        self.__combination = [(0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 1, 2),
+                              (3, 4, 5), (6, 7, 8), (0, 4, 8), (2, 4, 6)]
 
     @staticmethod
     def show_title():
@@ -47,22 +47,32 @@ class Game:
         self.box_list[select_box] = sign
 
     def game_move(self, player_obj):
-        # valid_input_list = [i for i in range(1, 10)]
         box_choose = int(input(
             "{} insert box number from above game board > ".format(player_obj.name)))
         if box_choose in self.box_list:
             player_obj.choices.append(box_choose - 1)
             self.update_box_list(box_choose - 1, player_obj.sign)
             player_obj.step += 1
-            # self.stop = self.game_state_check(player_obj)
-            self.draw_board()
+            self.game_state_check(player_obj)
+            if player_obj.win:
+                print("{}, Congrates! ".format(player_obj.name))
+                self.game_end()
+            else:
+                self.draw_board()
+
         else:
             print(
                 "\nInvalid input.Please check gameboard avaiable number and input again\n")
             self.game_move(player_obj)
 
     def game_state_check(self, player_obj):
-        pass
+        length = len(player_obj.choices)
+        if length >= 3:
+            choice_comb = perm(player_obj.choices, 3)
+            for comb in self.__combination:
+                if comb in choice_comb:
+                    player_obj.win = True
+                    break
 
     def game_start(self):
         self.draw_board()
@@ -77,6 +87,7 @@ class Player:
         self.sign = sign
         self.choices = []
         self.step = 0
+        self.win = False
 
     def show_player_info(self):
         print("{} choose {} sign".format(self.name, self.sign))
